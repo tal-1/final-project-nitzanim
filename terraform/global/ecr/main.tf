f0414bb055d4a9c0e742db1a652223870e68dc1e
tal-1 <143294035+tal-1@users.noreply.github.com>
2026-03-16 23:05:52 +0200

    adding image lifecycle policy in ecr

--- terraform/global/ecr/main.tf
+++ terraform/global/ecr/main.tf
@@ -10,3 +10,38 @@ resource "aws_ecr_repository" "django_app" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "django_app_cleanup" {
+  repository = aws_ecr_repository.django_app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Protect Production/Stage SemVer tags (v*)"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["v"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 9999 # Effectively keeps up to 9999 release images indefinitely
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Delete temporary Dev images (Commit IDs) and untagged images older than 14 days"
+        selection = {
+          tagStatus   = "any"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 14
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
