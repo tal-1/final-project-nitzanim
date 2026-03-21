44dd6dde509bddfda28b5fa0e653d5958a0fc41d
Noam Guy <noamg@hatal.org.il>
2026-03-21 13:46:29 +0200

    fix s3 output

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -7,3 +7,8 @@ output "alb_dns_name" {
   description = "The DNS name of the Application Load Balancer (for backend testing)"
   value       = module.alb.alb_dns_name
 }
+
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket holding the static files"
+  value       = module.frontend.s3_bucket_name
+}
