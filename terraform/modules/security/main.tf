3bd37c6a4bd94e2184f60bec6343ef9314b52f3d
tal-1 <talmolcho11@gmail.com>
2026-03-21 17:04:56 +0200

    add security group ingress rule for ElastiCache from ECS

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -95,6 +95,15 @@ resource "aws_vpc_security_group_egress_rule" "ecs_to_cache" {
   ip_protocol                  = "tcp"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "cache_from_ecs" {
+  security_group_id            = aws_security_group.cache.id
+  description                  = "Allow inbound ElastiCache traffic from ECS"
+  referenced_security_group_id = aws_security_group.ecs.id
+  from_port                    = 6379
+  to_port                      = 6379
+  ip_protocol                  = "tcp"
+}
+
 resource "aws_vpc_security_group_egress_rule" "ecs_to_internet_https" {
   security_group_id = aws_security_group.ecs.id
   description       = "Outbound HTTPS to Internet (For ECR Image Pulls)"
