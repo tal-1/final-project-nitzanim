400e97c17a5808ee1eeff4eb8b939ec52e702f1d
tal-1 <talmolcho11@gmail.com>
2026-03-21 14:46:54 +0200

    asdfghj

--- src/status-page/status-page/entrypoint.sh
+++ src/status-page/status-page/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # entrypoint.sh
 
-echo "Gathering static files..."
-python statuspage/manage.py collectstatic --noinput
-
-echo "Running database migrations..."
-python statuspage/manage.py migrate --noinput
-
 echo "Starting Gunicorn server..."
-# Start the server (adjust the path to gunicorn.py if needed)
-exec gunicorn -c statuspage/contrib/gunicorn.py statuspage.wsgi
+# Start the server
+exec gunicorn -c contrib/gunicorn.py statuspage.wsgi
