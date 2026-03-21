4fc15398e7f9833cdf166f637ade51c3bdf61e0c
tal-1 <talmolcho11@gmail.com>
2026-03-22 00:01:09 +0200

    fixing src/status-page/status-page/entrypoint.sh

--- src/status-page/status-page/entrypoint.sh
+++ src/status-page/status-page/entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 # If no arguments are passed from AWS, run the normal web server
 if [ $# -eq 0 ]; then
     echo "Starting Gunicorn server..."
-    exec gunicorn -c statuspage/contrib/gunicorn.py statuspage.wsgi
+    # 1. Point to the correct contrib folder
+    # 2. Tell Gunicorn to change directory into statuspage so it can find wsgi.py
+    exec gunicorn -c contrib/gunicorn.py --chdir statuspage statuspage.wsgi
 
 # If AWS passes a command (like "python manage.py migrate"), execute that instead!
 else
