6e7827a1912d4b70c24462da5dba28f4fd3d3654
tal-1 <talmolcho11@gmail.com>
2026-03-22 02:13:11 +0200

    fix stage cd pipeline

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -17,3 +17,23 @@ output "cloudfront_distribution_id" {
   description = "The ID of the CloudFront distribution"
   value       = module.frontend.cloudfront_distribution_id
 }
+
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = module.ecs.cluster_name
+}
+
+output "ecs_service_name" {
+  description = "The name of the ECS service"
+  value       = module.ecs.service_name
+}
+
+output "private_subnets" {
+  description = "The private subnets for the ECS tasks"
+  value       = module.networking.private_subnets
+}
+
+output "ecs_sg_id" {
+  description = "The security group ID for the ECS tasks"
+  value       = module.security.ecs_sg_id
+}
